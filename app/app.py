b98eb2bd38818fd2300eafa14922d53d66c626d2
Saruul Amarbayar <a.saruuld@gmail.com>
2023-08-26 13:32:46 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -79,4 +79,4 @@ app.layout = layout()
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=False)
